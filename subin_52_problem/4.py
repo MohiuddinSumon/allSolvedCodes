bb188055b3930ccbc8835369f20f5faa2cacc5b5
Mohiuddin Sumon <sumon13mp@gmail.com>
2023-01-01 20:21:46 +0600

    4th

--- subin_52_problem/4.py
+++ subin_52_problem/4.py
@@ -3,8 +3,7 @@ test_case = int(input())
 for i in range(1,test_case+1):
     number = int(input())
     print(f'Case {i}:', end=' ')
-    for j in range(1, number+1):
+    for j in range(1,number+1):
         if number%j == 0:
-            print(j , end=' ')
+            print(j, end=' ')
     print()
-    
