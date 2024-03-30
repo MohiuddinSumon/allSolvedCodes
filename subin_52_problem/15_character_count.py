5f5e759c49349d3880ed37dd0891825319d85c84
Mohiuddin Sumon <sumon13mp@gmail.com>
2024-03-30 13:40:37 +0600

    Update 15_character_count.py

--- subin_52_problem/15_character_count.py
+++ subin_52_problem/15_character_count.py
@@ -1,13 +1,27 @@
-# Reads two integers and prints their sum
-def basic_io():
-    a, b = map(int, input().split())
-    print(a + b)
+""" solved using set """
+
 
 def main():
+    """
+    This function takes user input and counts the frequency of each character in the input string.
+    It takes no parameters and does not return any values.
+    """
     test_case = int(input())
-    for _ in range(test_case):
+    for loop in range(test_case):
         S = input()
         letters = {}
         for i in S:
-            letters[S[i]]
+            letters[i] = letters.get(i, 0) + 1
+
+        for k, v in letters.items():
+            print(k, "=", v)
+        if test_case - 1 != loop:
+            print()
+
+
 main()
+
+
+# unique_chars = set(S)
+# +        for char in unique_chars:
+# +            print(char, '=', S.count(char))
